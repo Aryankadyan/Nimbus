bfbe61ab5e2ec82463b86b2982e5c608c62f4288
jai-max <kadyanaryan744@gmail.com>
2025-04-11 12:21:30 +0530

    React + Shadcn UI Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App(){
+  return (
+    <div>
+      Hey Bro
+      <Button variant="destructive">play the music</Button>
+    </div>
+   
+  )
+}
+
+export default App
